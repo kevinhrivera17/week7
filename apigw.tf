3fcc328b7d8909d6135de4bc64d58e66358c67dd
Kevin Rivera <khernandez@applaudostudios.dev>
2022-06-13 13:23:29 -0600

    Lamdbas working

---
+++ apigw.tf
@@ -0,0 +1,71 @@
+resource "aws_api_gateway_rest_api" "example" {
+  body = jsonencode({
+  "openapi" : "3.0.1",
+  "info" : {
+    "title" : "reminders",
+    "version" : "2022-06-13T18:10:50Z"
+  },
+  "paths" : {
+    "/reminders" : {
+      "post" : {
+        "responses" : {
+          "200" : {
+            "description" : "200 response",
+            "content" : {
+              "application/json" : {
+                "schema" : {
+                  "$ref" : "#/components/schemas/Empty"
+                }
+              }
+            }
+          }
+        },
+        "x-amazon-apigateway-integration" : {
+          "httpMethod" : "POST",
+          "uri" : aws_lambda_function.api_handler_lambda.invoke_arn,
+          "responses" : {
+            "default" : {
+              "statusCode" : "200"
+            }
+          },
+          "passthroughBehavior" : "when_no_match",
+          "contentHandling" : "CONVERT_TO_TEXT",
+          "type" : "aws"
+        }
+      }
+    }
+  },
+  "components" : {
+    "schemas" : {
+      "Empty" : {
+        "title" : "Empty Schema",
+        "type" : "object"
+      }
+    }
+  }
+})
+
+  name = "example"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_deployment" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.example.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "example" {
+  deployment_id = aws_api_gateway_deployment.example.id
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  stage_name    = "example"
+}
--- lambda.tf
+++ lambda.tf
@@ -15,3 +15,47 @@ data "archive_file" "zip-api-handler" {
   source_file = "lamdafiles/api-handler.py"
   output_path = "zipfiles/api-handler.zip"
 }
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "email_lambda" {
+  filename      = "zipfiles/email.zip"
+  function_name = "email"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler = "lambda-email.handler"
+  runtime = "python3.8"
+}
+
+resource "aws_lambda_function" "sms_lambda" {
+  filename      = "zipfiles/sms.zip"
+  function_name = "sms"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler = "lambda-sms.handler"
+  runtime = "python3.8"
+}
+
+resource "aws_lambda_function" "api_handler_lambda" {
+  filename      = "zipfiles/api-handler.zip"
+  function_name = "api-handler"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler = "lambda-apihandler.handler"
+  runtime = "python3.8"
+}
